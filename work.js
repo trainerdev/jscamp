a3d4f73c738932eb5da1ce2741fa2b4ddc5b2a50
Fatih Can ATA <94168554+trainerdev@users.noreply.github.com>
2022-01-30 00:45:03 +0300

    Create work.js

---
+++ work.js
@@ -0,0 +1,21 @@
+// İstediğimiz kadar sayı yolladığımız ve bu sayıların asal durumunu kontrol eden fonksiyon.
+
+
+function findPrime(...number) {
+  for (let i = 0; i < number.length; i++) {
+    let control = 0;
+    for (let j = 2; j < number[i]/2+1; j++) {
+      if (number[i] % j == 0) {
+        control++;
+        break;
+      }
+    }
+    if (control == 0) {
+      console.log(number[i] + " Bir Asal Sayıdır!");
+    } else {
+      console.log(number[i] + " Bir Asal Sayı Değildir!");
+    }
+  }
+}
+
+findPrime(15,23,7,9,36,17);
